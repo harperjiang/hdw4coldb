1f1b855234fc5f2e3afa3c1c0192031f973ce437
Harper Jiang <harperjiang@msn.com>
2016-11-22 16:20:59 -0600

    space

--- tool/space_analysis.c
+++ tool/space_analysis.c
@@ -13,16 +13,17 @@
 
 int main(int argc, char** argv) {
 
-	cht* ct = (cht*) malloc(sizeof(cht));
-	hashtable* ht = (hashtable*) malloc(sizeof(hashtable));
-
-	perf_buildcht(ct, argv[1]);
-	perf_buildhash(ht, argv[1]);
-
-	printf("CHT: bitmap size %u\n", ct->bitmap_size);
-	printf("CHT: payload size %u\n", ct->payload_size);
-	printf("CHT: overflow bkt size %u\n", ct->overflow->bucket_size);
-	printf("CHT: overflow size %u\n", ct->overflow->size);
-	printf("Hash: bucket size %u\n", ht->bucket_size);
+	if (atoi(argv[2]) == 1) {
+		cht* ct = (cht*) malloc(sizeof(cht));
+		perf_buildcht(ct, argv[1]);
+		printf("CHT: bitmap size %u\n", ct->bitmap_size);
+		printf("CHT: payload size %u\n", ct->payload_size);
+		printf("CHT: overflow bkt size %u\n", ct->overflow->bucket_size);
+		printf("CHT: overflow size %u\n", ct->overflow->size);
+	} else {
+		hashtable* ht = (hashtable*) malloc(sizeof(hashtable));
+		perf_buildhash(ht, argv[1]);
+		printf("Hash: bucket size %u\n", ht->bucket_size);
+	}
 }
 
