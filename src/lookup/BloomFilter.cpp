7530d53944555630e227101f6cb8bf4964721873
Harper <harperjiang@msn.com>
2016-12-27 00:51:25 -0600

    Bloom Filter

--- src/lookup/BloomFilter.cpp
+++ src/lookup/BloomFilter.cpp
@@ -26,23 +26,24 @@ BloomFilter::~BloomFilter() {
 void BloomFilter::build(kv* datas, uint data_size) {
 	uint bitnum = data_size * BF_BITMAP_FACTOR;
 	bitmapSize = bitnum / BF_BITMAP_UNIT + ((bitnum % BF_BITMAP_UNIT) ? 1 : 0);
-
+	bitnum = bitmapSize * BF_BITMAP_UNIT;
 	for (uint i = 0; i < size; i++) {
 		bitmaps[i] = new ulong[bitmapSize];
 	}
 
 	for (uint i = 0; i < data_size; i++) {
 		for (uint j = 0; j < size; j++) {
-			uint hashed = factors[j] * datas[i].key;
-			bitmaps[j][hashed / BF_BITMAP_UNIT] &= (1
+			uint hashed = (factors[j] * datas[i].key) % bitnum;
+			bitmaps[j][hashed / BF_BITMAP_UNIT] |= (1
 					<< (hashed % BF_BITMAP_UNIT));
 		}
 	}
 }
 
 bool BloomFilter::test(uint key) {
+	uint bitnum = bitmapSize * BF_BITMAP_UNIT;
 	for (uint i = 0; i < size; i++) {
-		uint hashed = factors[i] * key;
+		uint hashed = (factors[i] * key) % bitnum;
 		if (!bitmaps[i][hashed / BF_BITMAP_UNIT]
 				& (1 << hashed % BF_BITMAP_UNIT))
 			return false;
