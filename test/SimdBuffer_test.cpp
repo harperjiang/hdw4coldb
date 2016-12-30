b1e6161e5ce4eb64eea089e67cc490ecf2f0b126
Harper Jiang <harperjiang@msn.com>
2016-12-30 10:47:54 -0600

    more test cases

--- test/SimdBuffer_test.cpp
+++ test/SimdBuffer_test.cpp
@@ -20,6 +20,25 @@ TEST(SimdBuffer, Align) {
 	ASSERT_EQ(124141, _mm256_extract_epi32(result,2));
 	ASSERT_EQ(551, _mm256_extract_epi32(result,3));
 	ASSERT_EQ(4, size);
+
+	// 0000 1110
+	input = _mm256_setr_epi32(0, 0, 0, 0, 5242152, 124141, 551, 0);
+	result = SimdBuffer::align(input, &size);
+
+	ASSERT_EQ(5242152, _mm256_extract_epi32(result,0));
+	ASSERT_EQ(124141, _mm256_extract_epi32(result,1));
+	ASSERT_EQ(551, _mm256_extract_epi32(result,2));
+	ASSERT_EQ(3, size);
+
+	input = _mm256_setr_epi32(0, 0, 2719558, 0, 5242152, 124141, 551, 9895678);
+	result = SimdBuffer::align(input, &size);
+
+	ASSERT_EQ(2719558, _mm256_extract_epi32(result,0));
+	ASSERT_EQ(5242152, _mm256_extract_epi32(result,1));
+	ASSERT_EQ(124141, _mm256_extract_epi32(result,2));
+	ASSERT_EQ(551, _mm256_extract_epi32(result,3));
+	ASSERT_EQ(9895678, _mm256_extract_epi32(result,4));
+	ASSERT_EQ(5, size);
 }
 
 TEST(SimdBuffer, Shr) {
